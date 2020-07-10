d36937b9c68149783653f755057647fbecbdf070
Matt Ericksen <ericksenm93@ymail.com>
2020-07-10 18:06:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,5 +2,13 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
-    return "fizzbuzz"
+    return "FizzBuzz"
+  elsif int % 3 == 0
+    return "Fizz"
+  elsif int % 5 == 0
+    return "Buzz"
+  else
+    return nil
+  end
+end
     
